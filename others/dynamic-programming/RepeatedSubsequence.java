1afb7f84897ecfd721f8604936ee50e2c57a01bd
Rahul Pawar <rahulpawar1489@gmail.com>
2020-08-27 17:55:38 +0530

    Add print repeated subsequence

--- others/dynamic-programming/RepeatedSubsequence.java
+++ others/dynamic-programming/RepeatedSubsequence.java
@@ -53,6 +53,45 @@ class RepeatedSubsequence {
         checkInput(X);
         return recursion(X, X, X.length(), X.length());
     }
+
+    private static String getSequence(String X, int[][] table,
+                                        int length, int m, int n) {
+        char[] sequence = new char[length];
+
+        while (m > 0 && n > 0) {
+            if (X.charAt(m - 1) == X.charAt(n - 1) && m != n) {
+                sequence[--length] = X.charAt(m - 1);
+                m--;
+                n--;
+            } else if(table[m - 1][n] > table[m][n - 1]) {
+                m--;
+            } else {
+                n--;
+            }
+        }
+
+        return new String(sequence);
+    }
+
+    private static void sequenceLength(String X, int[][] table) {
+        for (int i = 1; i <= X.length(); ++i) {
+            for (int j = 1; j <= X.length(); ++j) {
+                if (X.charAt(i - 1) == X.charAt(j - 1) && i != j) {
+                    table[i][j] = 1 + table[i - 1][j - 1];
+                } else {
+                    table[i][j] = Math.max(table[i][j-  1], table[i - 1][j]);
+                }
+            }
+        }
+    }
+
+    public static String getSequence(String X) {
+        checkInput(X);
+        int n = X.length();
+        int[][] table = new int[n + 1][n + 1];
+        sequenceLength(X, table);
+        return getSequence(X, table, table[n][n], n, n);
+    }
 }
 
 class Driver {
@@ -70,13 +109,15 @@ class Driver {
     }
 
     public static void main(String[] args) {
-        int length = 10;
+        int length = 15;
         char[] alphabets = {'A', 'T', 'G', 'C'};
 
         String X = generateString(length, alphabets);
+        // String X = "ATACTCGGA";
         System.out.println("X : " + X);
 
         System.out.println("Using recursion : " + RepeatedSubsequence.recursion(X));
         System.out.println("Using top down : " + RepeatedSubsequence.topDown(X));
+        System.out.println("Sequence : " + RepeatedSubsequence.getSequence(X));
     }
 }
